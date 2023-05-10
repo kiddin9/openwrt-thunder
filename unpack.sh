0227ff21ef83b8f08c535f227a0e9f15f5b56a41
gngpp <gngppz@gmail.com>
2023-05-10 14:13:16 +0800

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+
+: ${arch:="$(uname -m)"}
+
 dir=$(pwd)/bin;
 if ! [ -d "$dir" ]; then
     mkdir $dir  
 fi
-pushd $dir
-:${arch:=$(uname -m)}
+
+download_dir=${pwd}/tmp;
+if ! [ -d "$download_dir" ]; then
+    mkdir $download_dir  
+fi
+
 if [ "$arch" = "aarch64" ]; then
  arch=armv8; 
 else
  arch=$(uname -m);
 fi
 
-wget https://down.sandai.net/nas/nasxunlei-DSM7-${arch}.spk
+filename="nasxunlei-DSM7-${arch}.spk"
 
+cd $download_dir
+if [ ! -f "$filename" ];then
+    wget https://down.sandai.net/nas/$filename
+fi
+cp $download_dir/$filename $dir/$filename
+cd -
+
+cd $dir
 tar --wildcards -Oxf $(find . -type f -name \*-${arch}.spk | head -n1) package.tgz | tar --wildcards -xJC ${dir} 'bin/bin/*' 'ui/index.cgi'
 mv ${dir}/bin/bin/* ${dir}/
 mv ${dir}/ui/index.cgi ${dir}/xunlei-pan-cli-web
@@ -20,4 +35,4 @@ rm -rf ${dir}/bin/bin
 rm -rf ${dir}/bin
 rm -rf ${dir}/ui
 rm -f ${dir}/version_code ${dir}/*.spk
-popd
+cd -
