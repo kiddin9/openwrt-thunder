e6c12bf9107cbbec7ed9752b3c3af3346ad99056
gngpp <gngppz@gmail.com>
2023-05-15 15:51:01 +0000

    support docker

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[ ! -d uploads ] && mkdir uploads;
-[ ! -d bin ] && mkdir bin;
+[ ! -d uploads ] && mkdir uploads
+[ ! -d bin ] && mkdir bin
 
 root=$(pwd)
 target_list=(x86_64-unknown-linux-gnu x86_64-unknown-linux-musl aarch64-unknown-linux-gnu aarch64-unknown-linux-musl)
@@ -13,10 +13,10 @@ for target in ${target_list[@]}; do
   cargo deb --target=$target --no-build --no-strip
   cargo generate-rpm --target=$target --payload-compress none
   cd target/$target/release
-  tar czvf xunlei-$tag.$target.tar.gz xunlei
-  shasum -a 256 xunlei-$tag.$target.tar.gz >xunlei-$tag.$target.tar.gz.sha256
-  mv xunlei-$tag.$target.tar.gz $root/uploads/
-  mv xunlei-$tag.$target.tar.gz.sha256 $root/uploads/
+  tar czvf xunlei-$tag-$target.tar.gz xunlei
+  shasum -a 256 xunlei-$tag-$target.tar.gz >xunlei-$tag-$target.tar.gz.sha256
+  mv xunlei-$tag-$target.tar.gz $root/uploads/
+  mv xunlei-$tag-$target.tar.gz.sha256 $root/uploads/
   cd -
   cd target/$target/debian
   rename 's/.*/xunlei-'$tag'.'$target'.deb/' *.deb
@@ -29,18 +29,19 @@ for target in ${target_list[@]}; do
 
   # embed feature
   if [[ $target == *"aarch64"* ]]; then
-    arch=aarch64 bash +x unpack.sh && cargo zigbuild --release --target=$target --no-default-features --features embed
+    arch=aarch64 bash +x unpack.sh
   else
-    bash +x unpack.sh && cargo zigbuild --release --target=$target --no-default-features --features embed
+    bash +x unpack.sh
   fi
+  cargo zigbuild --release --target=$target --no-default-features --features embed
   upx target/$target/release/xunlei
   cargo deb --target=$target --no-build --no-strip
   cargo generate-rpm --target=$target --payload-compress none
   cd target/$target/release
-  tar czvf xunlei-embed-$tag.$target.tar.gz xunlei
-  shasum -a 256 xunlei-embed-$tag.$target.tar.gz >xunlei-embed-$tag.$target.tar.gz.sha256
-  mv xunlei-embed-$tag.$target.tar.gz $root/uploads/
-  mv xunlei-embed-$tag.$target.tar.gz.sha256 $root/uploads/
+  tar czvf xunlei-embed-$tag-$target.tar.gz xunlei
+  shasum -a 256 xunlei-embed-$tag-$target.tar.gz >xunlei-embed-$tag-$target.tar.gz.sha256
+  mv xunlei-embed-$tag-$target.tar.gz $root/uploads/
+  mv xunlei-embed-$tag-$target.tar.gz.sha256 $root/uploads/
   cd -
   cd target/$target/debian
   rename 's/.*/xunlei-embed-'$tag'.'$target'.deb/' *.deb
@@ -50,6 +51,16 @@ for target in ${target_list[@]}; do
   rename 's/.*/xunlei-embed-'$tag'.'$target'.rpm/' xunlei*.rpm
   mv ./* $root/uploads/
   cd -
+
+  # launch feature
+  mkdir -p xunlei-launch-$tag-$target/bin
+  mv bin/* xunlei-launch-$tag-$target/bin/
+  cargo zigbuild --release --target=$target --no-default-features --features launch
+  mv target/$target/release/xunlei xunlei-launch-$tag-$target/
+  tar -czvf xunlei-launch-$tag-$target.tar.gz xunlei-launch-$tag-$target/*
+  shasum -a 256 xunlei-launch-$tag-$target.tar.gz >xunlei-launch-$tag-$target.tar.gz.sha256
+  mv xunlei-launch-$tag-$target.tar.gz uploads/
+  mv xunlei-launch-$tag-$target.tar.gz.sha256 uploads/
 
   rm -r bin/*
   ls -lah uploads
---
+++ docker/run_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -d "/var/packages/pan-xunlei-com/target/var" ]; then
+    id=$(cat /proc/sys/kernel/random/uuid | cut -c1-7)
+    echo "unique=\"synology_${id}_720+\"" > /var/packages/pan-xunlei-com/target/host/etc/synoinfo.conf
+fi
+mkdir -p /usr/syno/synoman/webman/modules
+ln -s /var/packages/pan-xunlei-com/target/host/etc/synoinfo.conf /etc/synoinfo.conf
+ln -s /var/packages/pan-xunlei-com/target/host/usr/syno/synoman/webman/modules/authenticate.cgi /usr/syno/synoman/webman/modules/authenticate.cgi
+
+mount --bind /bin /rootfs/bin
+mount --bind /run /rootfs/run
+mount --bind /lib /rootfs/lib
+mount --bind /lib64 /rootfs/lib64
+mount --bind /lib32 /rootfs/lib32
+mount --bind /libx32 /rootfs/libx32
+mount --bind /usr /rootfs/usr
+mount --bind /mnt /rootfs/mnt
+mount --bind /etc /rootfs/etc
+mount --bind /sbin /rootfs/sbin
+mount --bind /dev /rootfs/dev
+mount --bind /var /rootfs/var
+mount --bind /tmp /rootfs/tmp
+mount --bind /root /rootfs/root
+mount --bind /boot /rootfs/boot
+mount --bind /proc /rootfs/proc
+mount --bind /opt/xunlei /rootfs/opt/xunlei
+mount --bind /opt/xunlei/downloads /rootfs/opt/xunlei/downloads
+
+chroot /rootfs /bin/bash -c "echo 'nameserver 119.29.29.29' > /etc/resolv.conf && /usr/bin/mount -t proc none /proc && /usr/bin/xunlei launch"
+
+exec "$@"
