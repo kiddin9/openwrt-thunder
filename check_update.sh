bbb2ded875e8cc65b0d9f541eb8fdb6a1b1c2139
gngpp <gngppz@gmail.com>
2023-05-19 02:31:33 +0000

    Update check version

---
+++ check_update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# bash +x unpack.sh x86_64
+current_version="$(cat Cargo.toml | grep version | head -1 | awk -F= '{gsub(/[[:space:]"\/]/,"",$2); print substr($2, 1, index($2,"-")-1)}')"
+new_version="$(cat bin/version)"
+
+echo "current_version=$current_version"
+echo "new_version=$new_version"
+
+sed -i 's/'$current_version'/'$new_version'/g' Cargo.toml
+sed -i 's/'$current_version'/'$new_version'/g' openwrt/xunlei/Makefile
